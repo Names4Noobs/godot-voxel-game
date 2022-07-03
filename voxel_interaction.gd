99e78a96b71a9e683415c276573a7f4c303dea26
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2022-07-03 13:34:45 -0400

    Allow for a voxel to be selected in VoxelInteraction

--- voxel_interaction.gd
+++ voxel_interaction.gd
@@ -1,27 +1,38 @@
 extends Node
 
-@onready
-var camera = get_parent()
-@onready
-var terrain = get_node("../../VoxelTerrain")
 var voxel_tool: VoxelTool = null
+var voxel_library: VoxelBlockyLibrary = preload("res://data/voxel_library.tres")
+
+@onready var camera = get_parent()
+@onready var terrain: VoxelTerrain = get_node("../../VoxelTerrain")
+
+
+var selected_voxel := 1:
+	get: return selected_voxel
+	set(v):
+		selected_voxel = clamp(v, 1, voxel_library.voxel_count - 1)
+
 
 func _ready():
 	voxel_tool = terrain.get_voxel_tool()
 	voxel_tool.channel = VoxelBuffer.CHANNEL_TYPE
-	
+
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("place"):
 		voxel_tool.mode = VoxelTool.MODE_SET
 		var result = _get_pointed_voxel() 
 		if result != null:
-			voxel_tool.set_voxel(result.position, 1)
+			voxel_tool.set_voxel(result.position, selected_voxel)
 	elif Input.is_action_just_pressed("break"):
 		voxel_tool.mode = VoxelTool.MODE_REMOVE
 		var result = _get_pointed_voxel() 
 		if result != null:
 			voxel_tool.set_voxel(result.position, 0)
+	elif Input.is_action_just_released("scroll_up"):
+		selected_voxel += 1
+	elif Input.is_action_just_released("scroll_down"):
+		selected_voxel -= 1
 
 
 func _get_pointed_voxel() -> VoxelRaycastResult:
