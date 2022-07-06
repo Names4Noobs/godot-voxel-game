fa93fd483c39f86ad72d4915b86416a7e874e4ef
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2022-07-06 15:14:22 -0400

    Add basic camera switcher

---
+++ camera_switcher.gd
@@ -0,0 +1,17 @@
+extends Node
+
+#@onready var current_camera = get_viewport().get_camera_3d()
+
+@onready var camera1 = get_node("../FreeLookCamera")
+@onready var camera2 = get_node("../CharacterBody3D/Node3D/Camera3D")
+
+# TODO: Player body needs to be disabled when the cameras are switched
+
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("switch_camera"):
+		if camera1.current:
+			camera2.set_current(true)
+		elif camera2.current:
+			camera1.set_current(true)
