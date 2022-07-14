61d05b6b5630eca191a75def2fb68ca2dac7a58f
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2022-07-14 17:42:15 -0400

    VoxelGenerator: Make the terrible trees random!

--- misc/voxel_generator.gd
+++ misc/voxel_generator.gd
@@ -11,8 +11,10 @@ func _get_used_channels_mask() -> int:
 func _generate_block(buffer: VoxelBuffer, origin: Vector3i, lod: int) -> void:
 	if lod != 0:
 		return
-	
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
 	# Fill one chunk down
+	const chunk_depth = 16
 	if origin.y < 0 and origin.y > -17:
 		# Fill grass block for top layer
 		buffer.fill_area(2, Vector3i(0,15,0), Vector3i(16,16,16))
@@ -24,9 +26,9 @@ func _generate_block(buffer: VoxelBuffer, origin: Vector3i, lod: int) -> void:
 		buffer.fill(9, channel)
 	elif origin.y < -48 and origin.y > -65:
 		buffer.fill(10, channel)
-	elif origin.y < -64 and origin.y > -87:
+	elif origin.y < -64 and origin.y > -81:
 		buffer.fill(11, channel)
-	elif origin.y < -86 and origin.y > -103:
+	elif origin.y < -(chunk_depth * 10) and origin.y > -(chunk_depth * 11) - 1:
 		buffer.fill(12, channel)
 
 	if origin.x < 0 and origin.x > -17:
@@ -35,23 +37,29 @@ func _generate_block(buffer: VoxelBuffer, origin: Vector3i, lod: int) -> void:
 
 # Very crappy trees!!!!!
 	if origin.y > -1 and origin.y < 16:
-		if origin.x > 0:
-			# Create trunk
-			buffer.set_voxel(5, 2, 0, 2)
-			buffer.set_voxel(5, 2, 1, 2)
-			buffer.set_voxel(5, 2, 2, 2)
-			
-			# Add base leaves
-			buffer.fill_area(6, Vector3i(0, 3, 0), Vector3i(5, 5, 5))
-			buffer.fill_area(6, Vector3i(1, 5, 1), Vector3i(4, 6, 4))
-			
-			# Add top leaves
-			buffer.set_voxel(6, 2, 6, 2)
-			buffer.set_voxel(6, 2, 6, 1)
-			buffer.set_voxel(6, 3, 6, 2)
-			buffer.set_voxel(6, 1, 6, 2)
-			buffer.set_voxel(6, 2, 6, 3)
-			
-			# Fill in logs
-			buffer.set_voxel(5, 2, 3, 2)
-			buffer.set_voxel(5, 2, 4, 2)
+		if rng.randi_range(0, 3) == 3:
+			_create_tree(buffer, randi_range(2, 7))
+
+
+func _create_tree(buffer: VoxelBuffer, base: int) -> void:
+	# The base of the tree is the x,z location of the trunk
+	# Base must be >= 2 
+	# Create trunk
+	buffer.set_voxel(5, base, 0, base)
+	buffer.set_voxel(5, base, 1, base)
+	buffer.set_voxel(5, base, 2, base)
+	
+	# Add base leaves
+	buffer.fill_area(6, Vector3i(base-2, 3, base-2), Vector3i(base+3, 5, base+3))
+	buffer.fill_area(6, Vector3i(base-1, 5, base-1), Vector3i(base+2, 6, base+2))
+
+	# Add top leaves
+	buffer.set_voxel(6, base, 6, base)
+	buffer.set_voxel(6, base, 6, base-1)
+	buffer.set_voxel(6, base+1, 6, base)
+	buffer.set_voxel(6, base-1, 6, base)
+	buffer.set_voxel(6, base, 6, base+1)
+
+	# Fill in logs
+	buffer.set_voxel(5, base, 3, base)
+	buffer.set_voxel(5, base, 4, base)
