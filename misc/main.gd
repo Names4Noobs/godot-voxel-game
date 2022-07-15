83a1a6b656ff4b38498657f06ac744c3b98afb78
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2022-07-14 22:51:35 -0400

    Add functionality to the death screen

--- misc/main.gd
+++ misc/main.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 	terrain.generator = MyGenerator.new()
 
 
+# NOTE: This is just for testing!!
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		match event.keycode:
@@ -18,9 +19,10 @@ func _input(event: InputEvent) -> void:
 				Signals.emit_signal("player_died")
 				_on_player_died()
 
+
+# NOTE: This is just for testing!!
 func _on_player_died() -> void:
 	var screen = death_screen.instantiate()
 	add_child(screen)
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	get_tree().paused = true
+
 
---
+++ ui/death_screen.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+@onready var respawn_button = $VBoxContainer/Button
+@onready var quit_button = $VBoxContainer/Button2
+
+
+func _ready() -> void:
+	respawn_button.connect("pressed", Callable(self, "_on_respawn_button_pressed"))
+	quit_button.connect("pressed", Callable(func(): get_tree().quit()))
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	get_tree().paused = true
+
+
+func _on_respawn_button_pressed() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	get_tree().paused = false
+	queue_free()
