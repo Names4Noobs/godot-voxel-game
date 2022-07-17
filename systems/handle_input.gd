b7f8816d821a3a8eec28ab14abd0615df5120000
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2022-07-17 16:36:22 -0400

    Add take screenshot key

--- systems/handle_input.gd
+++ systems/handle_input.gd
@@ -14,4 +14,6 @@ var fullscreen = false:
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("toggle_fullscreen"):
 		fullscreen = !fullscreen
-
+	elif Input.is_action_just_pressed("take_screenshot"):
+		await RenderingServer.frame_post_draw
+		get_viewport().get_texture().get_image().save_png("user://%s-screenshot.png" % Time.get_date_string_from_system())
