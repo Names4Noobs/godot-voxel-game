11d7754fc920f66cf99e4862064c44ac5ed249e9
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2022-07-23 15:49:29 -0400

    Add explosion particles to tnt

--- entities/block_entity.gd
+++ entities/block_entity.gd
@@ -3,9 +3,11 @@ extends RigidDynamicBody3D
 var type = -1
 
 @onready var audio: AudioStreamPlayer3D = $AudioStreamPlayer3D
+@onready var anim_sprite: AnimatedSprite3D = $AnimatedSprite3D
 
 func _ready() -> void:
 	audio.connect("finished", Callable(self, "_on_audio_stream_player_3d_finished"))
+	anim_sprite.connect("animation_finished", Callable(self, "_on_animated_sprite_3d_animation_finished"))
 
 
 
@@ -17,6 +19,7 @@ func interact() -> bool:
 			print("This should smelt!")
 		Util.BlockEntity.TNT:
 			Signals.emit_signal("create_explosion", self.position, 10)
+			anim_sprite.play(&"explode")
 			audio.play()
 		_:
 			print("Block entity type unrecognized!")
@@ -24,4 +27,8 @@ func interact() -> bool:
 
 
 func _on_audio_stream_player_3d_finished() -> void:
+	pass
+
+
+func _on_animated_sprite_3d_animation_finished() -> void:
 	queue_free()
