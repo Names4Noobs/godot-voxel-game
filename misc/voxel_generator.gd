74ce1ee3d8d8c93b6a8bae4695257875facfc7ed
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2022-07-25 23:24:44 -0400

    Add biomes to world generator

--- misc/voxel_generator.gd
+++ misc/voxel_generator.gd
@@ -1,14 +1,19 @@
 extends VoxelGeneratorScript
 
 
+enum Biome {PLAINS, DESERT}
+
 const channel : int = VoxelBuffer.CHANNEL_TYPE
 const DEFAULT_WORLD_SEED = 123456
 
 var world_seed: int
+var _biome_noise := FastNoiseLite.new()
 
 
 func _init(p_world_seed=DEFAULT_WORLD_SEED) -> void:
 	world_seed = p_world_seed
+	_biome_noise.seed = world_seed
+	_biome_noise.frequency = 0.001
 
 
 func _get_used_channels_mask() -> int:
@@ -18,30 +23,58 @@ func _get_used_channels_mask() -> int:
 func _generate_block(buffer: VoxelBuffer, origin: Vector3i, lod: int) -> void:
 	if lod != 0:
 		return
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	_generate_plains(buffer, origin)
-	if origin.x > 100:
-		if origin.y < 0 and origin.y > -17:
-			buffer.fill_area(Util.Block.SAND, Vector3i.ZERO, Vector3i(16,16,16))
-	if origin.z > 100:
-		if origin.y < 0 and origin.y > -17:
-			buffer.fill_area(Util.Block.WATER, Vector3i.ZERO, Vector3i(16,16,16))
-# Very crappy trees!!!!!
-	if origin.z < 100 and origin.x < 100:
-		if origin.y > -1 and origin.y < 16:
-			if rng.randi_range(0, 3) == 3:
-				_create_tree(buffer, randi_range(2, 7))
+	var chunk_position := Vector3i(origin.x >> 4, origin.y >> 4, origin.z >> 4)
+	var biome := _get_chunk_biome(chunk_position.x, chunk_position.z)
+	match biome:
+		Biome.PLAINS:
+			_generate_plains(buffer, origin)
+		Biome.DESERT:
+			_generate_desert(buffer, origin)
 
+
+func _get_chunk_biome(x: int, z: int) -> int:
+	var result = _biome_noise.get_noise_2d(x * 16, z * 16)
+	if result < 0:
+		return Biome.PLAINS
+	else:
+		return Biome.DESERT
 
 
 func _generate_plains(buffer: VoxelBuffer, origin: Vector3i) -> void:
 	const chunk_depth = 16
+	var rng = RandomNumberGenerator.new()
+	rng.seed = world_seed
+	rng.randomize()
 
 	if origin.y < 0 and origin.y > -17:
 		# Fill grass block for top layer
 		buffer.fill_area(Util.Block.GRASS, Vector3i(0,15,0), Vector3i(16,16,16))
 		buffer.fill_area(Util.Block.DIRT, Vector3i(0,10,0), Vector3i(16,15,16))
+		buffer.fill_area(Util.Block.STONE, Vector3i.ZERO, Vector3i(16,10,16))
+
+	elif origin.y < -16 and origin.y > -33:
+		buffer.fill(Util.Block.COAL_ORE, channel)
+	elif origin.y < -32 and origin.y > -49:
+		buffer.fill(Util.Block.IRON_ORE, channel)
+	elif origin.y < -48 and origin.y > -65:
+		buffer.fill(Util.Block.GOLD_ORE, channel)
+	elif origin.y < -64 and origin.y > -81:
+		buffer.fill(Util.Block.DIAMOND_ORE, channel)
+	elif origin.y < -(chunk_depth * 10) and origin.y > -(chunk_depth * 11) - 1:
+		buffer.fill(Util.Block.LAVA, channel)
+	elif origin.y > -1 and origin.y < 16:
+		if rng.randi_range(0, 3) == 3:
+			_create_tree(buffer, randi_range(2, 7))
+
+
+
+
+func _generate_desert(buffer: VoxelBuffer, origin: Vector3i) -> void:
+	const chunk_depth = 16
+
+	if origin.y < 0 and origin.y > -17:
+		# Fill grass block for top layer
+		buffer.fill_area(Util.Block.SAND, Vector3i(0,10,0), Vector3i(16,16,16))
 		buffer.fill_area(Util.Block.STONE, Vector3i.ZERO, Vector3i(16,10,16))
 		
 	elif origin.y < -16 and origin.y > -33:
@@ -55,8 +88,12 @@ func _generate_plains(buffer: VoxelBuffer, origin: Vector3i) -> void:
 	elif origin.y < -(chunk_depth * 10) and origin.y > -(chunk_depth * 11) - 1:
 		buffer.fill(Util.Block.LAVA, channel)
 
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
 
-
+	if origin.y > -1 and origin.y < 16:
+		if rng.randi_range(0, 3) == 3:
+			_create_tree(buffer, randi_range(2, 7))
 
 
 func _create_tree(buffer: VoxelBuffer, base: int) -> void:
