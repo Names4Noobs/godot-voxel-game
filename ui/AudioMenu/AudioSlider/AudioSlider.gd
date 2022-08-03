bc44b1495528b6fd914e428ce9ff94f05bc821f8
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2022-08-03 00:55:57 -0400

    Create audio slider node

---
+++ ui/AudioMenu/AudioSlider/AudioSlider.gd
@@ -0,0 +1,37 @@
+@tool
+extends VBoxContainer
+
+@export var audio_bus: StringName = &"Master":
+	set(v):
+		if Engine.is_editor_hint():
+			label.set_text(_get_formated_string(1.0))
+		else:
+			_bus_index = AudioServer.get_bus_index(audio_bus)
+		audio_bus = v
+
+var _bus_index: int 
+
+@onready var label: Label = $Label
+@onready var slider: HSlider = $HSlider
+
+
+func _ready() -> void:
+	if !Engine.is_editor_hint():
+		_bus_index = AudioServer.get_bus_index(audio_bus)
+		slider.connect("value_changed", Callable(self, "_on_slider_value_changed"))
+		_update_ui()
+
+
+func _update_ui() -> void:
+	var volume = db2linear(AudioServer.get_bus_volume_db(_bus_index))
+	slider.value = volume
+	label.set_text(_get_formated_string(volume))
+
+
+func _on_slider_value_changed(value: float) -> void:
+	label.set_text(str(audio_bus) + " Volume: " + str(value)) 
+	AudioServer.set_bus_volume_db(_bus_index, linear2db(value))
+
+
+func _get_formated_string(value: float) -> String:
+	return str(audio_bus) + " Volume: " + str(value)
--- ui/AudioMenu/audio_menu.gd
+++ ui/AudioMenu/audio_menu.gd
@@ -1,36 +1,11 @@
 extends Control
-# TODO: Make an audio slider node
+
 
 var master_audio_bus := &"Master"
 var master_bus_index = AudioServer.get_bus_index(master_audio_bus)
-var ui_audio_bus := &"UI"
-var ui_bus_index = AudioServer.get_bus_index(ui_audio_bus)
-
 
 @onready var mute_button: CheckButton = $MarginContainer/VBoxContainer/CheckButton
-@onready var master_volume_slider: HSlider = $MarginContainer/VBoxContainer/MasterVolume/HSlider
-@onready var master_volume_label: Label = $MarginContainer/VBoxContainer/MasterVolume/Label
-@onready var ui_volume_label: Label = $MarginContainer/VBoxContainer/UIVolume/Label
-@onready var ui_volume_slider: HSlider = $MarginContainer/VBoxContainer/UIVolume/HSlider
 
 
 func _ready() -> void:
 	mute_button.connect("toggled", Callable(func(v): AudioServer.set_bus_mute(master_bus_index, v)))
-	master_volume_slider.connect("value_changed", Callable(self, "_on_master_volume_changed"))
-	master_volume_slider.value = db2linear(AudioServer.get_bus_volume_db(master_bus_index))
-	master_volume_label.text = "Master Volume: " + str(master_volume_slider.value)
-	ui_volume_slider.connect("value_changed", Callable(self, "_on_ui_volume_changed"))
-	ui_volume_slider.value = db2linear(AudioServer.get_bus_volume_db(ui_bus_index))
-	ui_volume_label.text = "UI Volume: " + str(ui_volume_slider.value)
-
-
-
-
-func _on_master_volume_changed(value: float) -> void:
-	master_volume_label.text = "Master Volume: " + str(value)
-	AudioServer.set_bus_volume_db(master_bus_index, linear2db(value))
-
-
-func _on_ui_volume_changed(value: float) -> void:
-	ui_volume_label.text = "UI Volume: " + str(value)
-	AudioServer.set_bus_volume_db(ui_bus_index, linear2db(value))
