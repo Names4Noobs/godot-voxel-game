8de5da0614eee78cc225ff8922c6e05950954b81
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2022-08-10 01:56:44 -0400

    Make voxel generator generate biomes

--- generation/voxel_generator.gd
+++ generation/voxel_generator.gd
@@ -3,7 +3,7 @@ extends VoxelGeneratorScript
 
 enum Biome {PLAINS, DESERT}
 
-const channel : int = VoxelBuffer.CHANNEL_TYPE
+const channel: int = VoxelBuffer.CHANNEL_TYPE
 const DEFAULT_WORLD_SEED = 123456
 
 var world_seed: int
@@ -14,6 +14,7 @@ func _init(p_world_seed=DEFAULT_WORLD_SEED) -> void:
 	world_seed = p_world_seed
 	_biome_noise.seed = world_seed
 	_biome_noise.frequency = 0.001
+	_biome_noise.fractal_octaves = 4
 
 
 func _get_used_channels_mask() -> int:
@@ -23,22 +24,33 @@ func _get_used_channels_mask() -> int:
 func _generate_block(out_buffer: VoxelBuffer, origin_in_voxels: Vector3i, lod: int) -> void:
 	if lod != 0:
 		return
+	var rng = RandomNumberGenerator.new()
+	rng.seed = world_seed
+	rng.randomize()
 	var _block_size := int(out_buffer.get_size().x)
-	var _chunk_position := Vector3i(origin_in_voxels.x >> 4, origin_in_voxels.y >> 4, origin_in_voxels.z >> 4)
+#	var _chunk_position := Vector3i(origin_in_voxels.x >> 4, origin_in_voxels.y >> 4, origin_in_voxels.z >> 4)
 	
-	_generate_plains(out_buffer, origin_in_voxels)
+	var gz = origin_in_voxels.z
+	var gx: int
+	if origin_in_voxels.y < 1 and origin_in_voxels.y > -16:
+		for z in _block_size:
+			gx = origin_in_voxels.x
+			for x in _block_size:
+				if _get_chunk_biome(gz, gx) == Biome.PLAINS:
+					out_buffer.set_voxel(2, x, 0, z, channel)
+				else:
+					out_buffer.set_voxel(3, x, 0, z, channel)
+					gx += 1
+			gz += 1
 
-#	var biome := _get_chunk_biome(chunk_position.x, chunk_position.z)
-#	match biome:
-#		Biome.PLAINS:
-#			_generate_plains(out_buffer, origin_in_voxels)
-#		Biome.DESERT:
-#			_generate_desert(out_buffer, origin_in_voxels)
+	else:
+		_generate_plains(out_buffer, origin_in_voxels)
+
 
 
 func _get_chunk_biome(x: int, z: int) -> int:
-	var result = _biome_noise.get_noise_2d(x * 16, z * 16)
-	if result < 0:
+	var result = 0.5 + 0.5 * _biome_noise.get_noise_2d(x, z)
+	if result < 0.5:
 		return Biome.PLAINS
 	else:
 		return Biome.DESERT
@@ -66,38 +78,9 @@ func _generate_plains(buffer: VoxelBuffer, origin: Vector3i) -> void:
 		buffer.fill(Util.Block.DIAMOND_ORE, channel)
 	elif origin.y < -(chunk_depth * 10) and origin.y > -(chunk_depth * 11) - 1:
 		buffer.fill(Util.Block.LAVA, channel)
-	elif origin.y > -1 and origin.y < 16:
-		if rng.randi_range(0, 3) == 3:
-			_create_tree(buffer, randi_range(2, 7))
-
-
-
-
-func _generate_desert(buffer: VoxelBuffer, origin: Vector3i) -> void:
-	const chunk_depth = 16
-
-	if origin.y < 0 and origin.y > -17:
-		# Fill grass block for top layer
-		buffer.fill_area(Util.Block.SAND, Vector3i(0,10,0), Vector3i(16,16,16))
-		buffer.fill_area(Util.Block.STONE, Vector3i.ZERO, Vector3i(16,10,16))
-		
-	elif origin.y < -16 and origin.y > -33:
-		buffer.fill(Util.Block.COAL_ORE, channel)
-	elif origin.y < -32 and origin.y > -49:
-		buffer.fill(Util.Block.IRON_ORE, channel)
-	elif origin.y < -48 and origin.y > -65:
-		buffer.fill(Util.Block.GOLD_ORE, channel)
-	elif origin.y < -64 and origin.y > -81:
-		buffer.fill(Util.Block.DIAMOND_ORE, channel)
-	elif origin.y < -(chunk_depth * 10) and origin.y > -(chunk_depth * 11) - 1:
-		buffer.fill(Util.Block.LAVA, channel)
-
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-
-	if origin.y > -1 and origin.y < 16:
-		if rng.randi_range(0, 3) == 3:
-			_create_tree(buffer, randi_range(2, 7))
+#	elif origin.y > -1 and origin.y < 16:
+#		if rng.randi_range(0, 3) == 3:
+#			_create_tree(buffer, randi_range(2, 7))
 
 
 func _create_tree(buffer: VoxelBuffer, base: int) -> void:
