f9cf7c85395c3c623f7f1c2ae88c24e3df9d67b0
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2023-03-01 20:54:01 -0500

    Add player and voxel terrain

---
+++ entities/player/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var head := $CameraHead
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var new_basis: Basis = head.basis.rotated(head.basis.x, -head.basis.get_euler().x)
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction := (new_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
---
+++ misc/freelook_camera/freelook_camera.gd
@@ -0,0 +1,39 @@
+# Based on:
+# Copyright © 2022 Marc Nahr: https://github.com/MarcPhi/godot-free-look-camera
+extends Camera3D
+
+@export_range(0, 10, 0.01) var sensitivity: float = 3
+@export_range(0, 1000, 0.1) var default_velocity: float = 5
+@export_range(0, 10, 0.01) var speed_scale: float = 1.17
+@export var max_speed: float = 1000
+@export var min_speed: float = 0.2
+
+@onready var _velocity = default_velocity
+
+
+func _ready() -> void:
+	if current:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _input(event: InputEvent):
+	if not current:
+		return
+	
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			rotation.y -= event.relative.x / 1000 * sensitivity
+			rotation.x -= event.relative.y / 1000 * sensitivity
+			rotation.x = clamp(rotation.x, PI/-2, PI/2)
+	
+	if event is InputEventMouseButton:
+		match event.button_index:
+			MOUSE_BUTTON_WHEEL_UP: # increase fly velocity
+				_velocity = clamp(_velocity * speed_scale, min_speed, max_speed)
+			MOUSE_BUTTON_WHEEL_DOWN: # decrease fly velocity
+				_velocity = clamp(_velocity / speed_scale, min_speed, max_speed)
+
+
+func _process(delta: float):
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back").normalized()
+	translate(Vector3(input_dir.x, 0, input_dir.y) * _velocity * delta)
