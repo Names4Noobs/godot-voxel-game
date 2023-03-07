66f0992a14979a719c4b73cde1eae0d31cacf536
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2023-03-06 19:23:14 -0500

    Improve player hand animations

--- misc/player_hand/player_hand.gd
+++ misc/player_hand/player_hand.gd
@@ -3,26 +3,50 @@ extends Node3D
 var inventory: Inventory
 @onready var block_animation_player := $CSGBox3D/AnimationPlayer
 @onready var hand_animation_player := $hand/AnimationPlayer
+@onready var hand := $hand
+@onready var block_hand := $CSGBox3D	
 
 func _ready() -> void:
 	Game.connect("block_placed", _on_block_placed)
 	inventory = Game.player_inventory
 	inventory.connect("selected_slot_changed", _on_selected_slot_changed)
-	var slot := inventory.get_selected_slot()
-	if not slot.is_empty():
-		$CSGBox3D.material_override.albedo_texture = slot.item.texture
-
-func _on_selected_slot_changed(_new_slot_id: int) -> void:
-	var slot := inventory.get_selected_slot()
-	if not slot.is_empty():
-		$CSGBox3D.material_override.albedo_texture = slot.item.texture
+	_on_selected_slot_changed(420)
 
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_released("primary_action"):
+		hand_animation_player.stop()
 		hand_animation_player.play("swing")
+		block_animation_player.stop()
+		block_animation_player.play("swing")
+
+
+func _update_player_hand(slot: ItemStack) -> void:
+	if not slot.is_empty():
+		if slot.item is BlockItem:
+			hand.hide()
+			block_hand.material_override.albedo_texture = slot.item.texture
+			block_hand.show()
+			return
+	block_hand.hide()
+	hand.show()
+
+
+func _on_selected_slot_changed(_new_slot_id: int) -> void:
+	var slot := inventory.get_selected_slot()
+	if not slot.is_connected("item_changed", _on_item_changed):
+		slot.connect("item_changed", _on_item_changed)
+	_update_player_hand(slot)
 
 
 func _on_block_placed() -> void:
+	block_animation_player.stop()
 	block_animation_player.play("place")
-	
+
+
+func _on_item_changed(_item: Item) -> void:
+	var slot := inventory.get_selected_slot()
+	_update_player_hand(slot)
+
+
+
