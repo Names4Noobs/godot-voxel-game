3e1c110e831c08a5f8f757685315df276314030e
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2023-04-03 20:40:28 -0400

    Prototype save games
    This is just a rough sketch of what saving will look like..........

--- world.gd
+++ world.gd
@@ -6,17 +6,46 @@ const PlayerEntity := preload("res://entities/player/player.tscn")
 const ZombieEntity := preload("res://entities/zombie/zombie.tscn")
 const CowEntity := preload("res://entities/cow/cow.tscn")
 
+
+var world_name := &"test"
+var world_seed: int
+var ticks: int
+var gamemode := 0
+
 @onready var voxel_terrain := $VoxelTerrain
 @onready var sun := $Sun
 
 
 func _ready() -> void:
+	var save_path := "user://saves/%s/" % world_name
 	voxel_terrain.generator = FlatlandGenerator.new()
+	if not DirAccess.dir_exists_absolute(save_path+"regions"):
+		DirAccess.make_dir_recursive_absolute(save_path+"regions")
+	# NOTE: I think streams are broken on the build I am using
+	#var stream := VoxelStreamRegionFiles.new()
+	#stream.directory = save_path+"regions"
+	#voxel_terrain.stream = stream
 	_spawn_player()
 
 
 func _physics_process(_delta: float) -> void:
+	ticks += 1
 	sun.rotate_x(0.001)
+
+
+func save_game() -> void:
+	var save_path := "user://saves/%s/" % world_name
+	if not DirAccess.dir_exists_absolute(save_path):
+		DirAccess.make_dir_recursive_absolute(save_path)
+	var file := FileAccess.open(save_path+"world.dat", FileAccess.WRITE)
+	if file:
+		file.store_line(JSON.stringify(get_data()))
+	else:
+		printerr(FileAccess.get_open_error())
+	
+
+func load_game() -> void:
+	pass
 
 
 func create_item_drop(drop_position: Vector3, item_stack: ItemStack) -> ItemDrop:
@@ -48,3 +77,21 @@ func _spawn_player() -> void:
 	player.position = Vector3(0, 4.0, 0)
 	add_child(player)
 	Events.emit_signal("player_spawned", player)
+
+
+func get_data() -> Dictionary:
+	return {
+		"world_name": world_name,
+		"generator": "flat",
+		"world_seed": world_seed,
+		"world_time": ticks,
+		"gamemode": gamemode,
+		"world_border_size": 100000,
+		"gamerules": {},
+		"last_played": Time.get_unix_time_from_system(),
+		"version": "Dev"
+	}
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		save_game()
